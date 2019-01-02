c93978a874edb39e38d591ed31594238d31b0aa1
Jura <jurakulek@gmail.com>
2019-01-02 14:24:43 +0200

    create new login template, use LoginView

--- user/views.py
+++ user/views.py
@@ -12,10 +12,9 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             # this message will show us success of create our user
-            messages.success(request, f'Account created for {username}!')
-            return redirect('home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
