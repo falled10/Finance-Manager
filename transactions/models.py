a79d85b9f4695832c6881c94b870a6062227e523
Jura <jurakulek@gmail.com>
2019-01-03 13:33:38 +0200

    create model for transaction

--- transactions/models.py
+++ transactions/models.py
@@ -7,3 +7,14 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name} category'
+
+
+class Transaction(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    operation_type = models.CharField(max_length=200)
+    money = models.DecimalField(decimal_places=2, max_digits=10000)
+    brief_desctiption = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return f'{self.brief_desctiption}'
