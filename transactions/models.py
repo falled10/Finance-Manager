792ee8008860d9ecd0cfc19dbbef85905e777f6a
Jura <jurakulek@gmail.com>
2019-01-04 13:28:48 +0200

    add many-to-one relatioship to transaction and category models

--- transactions/models.py
+++ transactions/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     brief_description = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -19,6 +21,7 @@ class Transaction(models.Model):
     money = models.DecimalField(decimal_places=2, max_digits=10000)
     brief_description = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.brief_description}'
--- transactions/views.py
+++ transactions/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.db.models import ProtectedError
 from django.views.generic import (
     CreateView,
@@ -16,10 +17,20 @@ class CategoryCreateView(CreateView):
     template_name = 'transactions/category_create.html'
     form_class = CategoryCreateForm
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
+
 
 class TransactionCreateView(CreateView):
     template_name = 'transactions/transaction_create.html'
     form_class = TransactionCreateForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
 
 
 class TransactionUpdateView(UpdateView):
@@ -28,6 +39,7 @@ class TransactionUpdateView(UpdateView):
     queryset = Transaction.objects.all()
 
     def form_valid(self, form):
+        form.instance.user = self.request.user
         return super().form_valid(form)
 
 
@@ -37,12 +49,16 @@ class CategoryUpdateView(UpdateView):
     queryset = Category.objects.all()
 
     def form_valid(self, form):
+        form.instance.user = self.request.user
         return super().form_valid(form)
 
 
 class CategoryListView(ListView):
     template_name = 'transactions/all_categories.html'
-    queryset = Category.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        return Category.objects.filter(user=user)
 
     def post(self, request, *args, **kwargs):
         category = request.POST['category']
@@ -56,6 +72,10 @@ class CategoryListView(ListView):
 class TransactionListView(ListView):
     template_name = 'transactions/all_transactions.html'
     queryset = Transaction.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(user=user)
 
     def post(self, request, *args, **kwargs):
         transaction = request.POST['transaction']
@@ -90,8 +110,8 @@ class CategoryDeleteView(DeleteView):
 
 def generate_spline(request):
     context = {
-        'option_list': Transaction.objects.all(),
-        'categories_list': Category.objects.all(),
+        'option_list': Transaction.objects.filter(user=request.user),
+        'categories_list': Category.objects.filter(user=request.user),
     }
     if request.method == 'POST':
         try:
@@ -100,7 +120,7 @@ def generate_spline(request):
             option = request.POST['option']
             category = request.POST['category']
             category = Category.objects.get(name=category)
-            data = Transaction.objects.filter(category=category, operation_type=option, pub_date__range=(start, end))
+            data = Transaction.objects.filter(user=request.user, category=category, operation_type=option, pub_date__range=(start, end))
             data_for_graph = [[i.pub_date.strftime('%Y-%m-%d'), float(i.money)] for i in data]
             print(data_for_graph)
             context['data'] = json.dumps(data_for_graph)
@@ -113,15 +133,15 @@ def generate_spline(request):
 
 def generate_pie(request):
     context = {
-        'option_list': Transaction.objects.all(),
-        'categories_list': Category.objects.all(),
+        'option_list': Transaction.objects.filter(user=request.user),
+        'categories_list': Category.objects.filter(user=request.user),
     }
     if request.method == 'POST':
         try:
             start = request.POST['start']
             end = request.POST['end']
             option = request.POST['option']
-            data = Transaction.objects.filter(operation_type=option, pub_date__range=(start, end))
+            data = Transaction.objects.filter(user=request.user, operation_type=option, pub_date__range=(start, end))
             categories = Category.objects.all()
             data_for_graph = []
             for category in categories:
